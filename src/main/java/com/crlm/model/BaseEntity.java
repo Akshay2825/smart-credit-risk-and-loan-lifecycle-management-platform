4463c6a1e91d753268f770441f6139021d5dae0f
Vaishnavi Kolhe <vaishnavikolhe22@gmail.com>
2026-01-22 00:43:47 +0530

    base entity added

---
+++ src/main/java/com/crlm/model/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.crlm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+}

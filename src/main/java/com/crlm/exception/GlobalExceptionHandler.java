7c49a92bce6194dc69e6cd9f72ba932a71981d93
vaishnavi-1619 <vaishnavikolhe22@gmail.com>
2026-01-30 01:52:59 +0530

    added exception handling

---
+++ src/main/java/com/crlm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.crlm.exception;
+
+import com.crlm.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private  ResponseEntity<ErrorResponse> buildResponse(HttpStatus status, String message){
+        return ResponseEntity.status(status).body(new ErrorResponse(status.value(), message,Instant.now()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex){
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), Instant.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    };
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument( IllegalArgumentException ex){
+        ErrorResponse error= new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),Instant.now());
+        return  ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex){
+        ErrorResponse error= new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server error",Instant.now());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserExists(UserAlreadyExistsException ex){
+        return buildResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+}
---
+++ src/main/java/com/crlm/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.crlm.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
